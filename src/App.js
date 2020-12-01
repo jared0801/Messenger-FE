74f2b6e02ce7ba99cc1c81e674d5c0b51e899154
jacobs67 <jared0801@gmail.com>
2020-11-30 16:58:09 -0800

    Revert back to browserrouter - using server side redirect to prevent users manually changing name/room in url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Join from './components/Join/Join';
 import Chat from './components/Chat/Chat';
